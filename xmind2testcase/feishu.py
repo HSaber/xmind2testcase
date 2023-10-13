7e778288905a9f40b9dde7894018eca6aee1eac8
huhuan <604490051@qq.com>
2023-10-13 15:37:36 +0800

    feishu

--- xmind2testcase/feishu.py
+++ xmind2testcase/feishu.py
@@ -19,10 +19,8 @@ def xmind_to_feishu_csv_file(xmind_file):
     testcases = get_xmind_testcase_list(xmind_file)
 
 #    fileheader = ["所属模块", "用例标题", "前置条件", "步骤", "预期", "关键词", "优先级", "用例类型", "适用阶段","负责人"]
-    fileheader = ["工作项类型","用例名称", "业务线","描述","前置条件",
-                  "执行步骤", "预期结果", "附件","标签", "步骤",
-                  "结果预期", "用例分级","关联需求","用例类型", "创建人",
-                  "优先级","关注人","拉群方式选择","流程角色","状态"]
+    fileheader = ["工作项类型","用例名称", "业务线","描述","前置条件","执行步骤", "预期结果", "附件","标签", "步骤","结果预期",
+                  "用例分级","关联需求","用例类型", "创建人","优先级","关注人","拉群方式选择","流程角色","状态"]
 
     feishu_testcase_rows = [fileheader]
     feishu_testcase_rows.append(['','','','','','','','','','','','','','','','','','','负责人',''])
@@ -36,7 +34,7 @@ def xmind_to_feishu_csv_file(xmind_file):
         # logging.info('The feishu csv file already exists, return it directly: %s', feishu_file)
         # return feishu_file
 
-    with open(feishu_file, 'w', encoding='utf8') as f:
+    with open(feishu_file, 'w',newline='', encoding='utf8') as f:
         writer = csv.writer(f)
         writer.writerows(feishu_testcase_rows)
         logging.info('Convert XMind file(%s) to a feishu csv file(%s) successfully!', xmind_file, feishu_file)
@@ -45,9 +43,17 @@ def xmind_to_feishu_csv_file(xmind_file):
 
 
 def gen_a_testcase_row(testcase_dict):
-    case_title = gen_case_module(testcase_dict['suite'])+"-"+testcase_dict['name']
+    product=gen_case_module(testcase_dict['product'])
+    logging.info(product)
+    if product.__contains__('_'):
+        case_story=product.split('_')[0]
+        case_owner=product.split('_')[1]
+    else:
+        case_story=product
+        case_owner=''
+    case_title = gen_case_module(testcase_dict['suite'])+">"+testcase_dict['name']
     case_jobtype='默认测试用例类型'
-    case_business='交易'
+    case_business='云卖单'
     case_description=''
     case_precontion = testcase_dict['preconditions']
     case_step, case_expected_result = gen_case_step_and_expected_result(testcase_dict['steps'])
@@ -56,14 +62,13 @@ def gen_a_testcase_row(testcase_dict):
     case_priority = gen_case_priority(testcase_dict['importance'])
     case_type = gen_case_type(testcase_dict['execution_type'])
  #   case_type= '功能测试'
-    case_owner='胡欢'
-    case_story='15715591'
     case_group='不拉群'
     case_status='待评审'
     row = [case_jobtype, case_title, case_business,case_description,case_precontion,
            case_step,case_expected_result, case_attach,case_tag,case_step,
            case_expected_result,case_priority, case_story, case_type,case_owner,
            case_priority,case_owner,case_group,case_owner,case_status]
+    # logging.info('row : %s',row)
     return row
 
 def gen_case_module(module_name):
@@ -95,13 +100,21 @@ def gen_case_priority(priority):
     else:
         return 'P1'
 
-
 def gen_case_type(case_type):
+
     mapping = {1: '功能用例', 2: '性能用例'}
+ #   return caseowener=case_type.split('_')
     if case_type in mapping.keys():
         return mapping[case_type]
     else:
         return '功能用例'
+
+# def gen_case_type(case_type):
+#     mapping = {1: '功能用例', 2: '性能用例'}
+#     if case_type in mapping.keys():
+#         return mapping[case_type]
+#     else:
+#         return '功能用例'
 
 
 if __name__ == '__main__':
